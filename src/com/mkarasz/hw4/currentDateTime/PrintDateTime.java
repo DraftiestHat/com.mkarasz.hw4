e16c148082bfe29ec38549d0215799d522f5e73b
DraftiestHat <mattchris9@hotmail.com>
2015-02-21 12:01:36 -0700

    Finished time, part into MIDI
    Time is finished. Could go back and put into functions, but no. MIDI is
    slowly being worked through. Converting a note representation in SPN to
    MIDI notes

--- src/com/mkarasz/hw4/currentDateTime/PrintDateTime.java
+++ src/com/mkarasz/hw4/currentDateTime/PrintDateTime.java
@@ -1,12 +1,79 @@
 package com.mkarasz.hw4.currentDateTime;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
 
 
 public class PrintDateTime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String curTime = new SimpleDateFormat("MM/dd/yy hh:mm a").format(Calendar.getInstance().getTime());
+		Calendar cal = new GregorianCalendar();
+		
+		System.out.println(curTime);
+		
+		switch(cal.get(Calendar.DAY_OF_WEEK)){
+		case 1:
+			System.out.print("Sunday, ");
+			break;
+		case 2:
+			System.out.print("Monday, ");
+			break;
+		case 3:
+			System.out.print("Tuesday, ");
+			break;
+		case 4:
+			System.out.print("Wednesday, ");
+			break;
+		case 5:
+			System.out.print("Thursday, ");
+			break;
+		case 6:
+			System.out.print("Friday, ");
+			break;
+		case 7:
+			System.out.print("Saturday, ");
+			break;
+		}
+		
+		
+		System.out.print("" + cal.get(Calendar.MONTH) + "." + cal.get(Calendar.DAY_OF_MONTH) + "." + cal.get(Calendar.YEAR) + " ");
+		System.out.print("" + cal.get(Calendar.HOUR) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND));
+		
+		switch(cal.get(Calendar.AM)){
+		case 1:
+			System.out.println(" AM");
+			break;
+		default:
+			System.out.println(" PM");
+		}
+		
+		System.out.print("Which is the " + cal.get(Calendar.DAY_OF_YEAR));
+		
+		switch(cal.get(Calendar.DAY_OF_YEAR) % 10){
+		case 0:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+			System.out.print("th");
+			break;
+		case 1:
+			System.out.print("st");
+			break;
+		case 2:
+			System.out.print("nd");
+			break;
+		case 3:
+			System.out.print("rd");
+			break;
+		}
+		
+		System.out.println(" day of the year.");
+	
 	}
 
 }
--- src/com/mkarasz/hw4/sound/MIDI.java
+++ src/com/mkarasz/hw4/sound/MIDI.java
@@ -1,5 +1,120 @@
 package com.mkarasz.hw4.sound;
 
 public class MIDI {
+	public enum Length { 
+		WHOLE, HALF, QUARTER, EIGHTH, SIXTEENTH, THIRTYSECONDTH, SIXTYFOURTH
+	}
+	
+	private enum Note {
+		C, CSHARP, D, DSHARP, E, F, FSHARP, G, GSHARP, A, ASHARP, B
+	}
+	
+	Length length;
+	int midiNumber = 0;
+	
+	public MIDI(){
+		setNoteDefault();
+	}
+	
+	public MIDI(String note, Length length){
+		int i = 0;
+		this.length = length;
+		
+		if(note.matches("^[A-G]{1}[#]?[-]?[0-9]{1}$")) {
+			if(note.matches("[A-G]{1}[#]?-[2-9]{1}")){ //checking for octaves less than -1
+				setNoteDefault();
+				System.out.println("Your note " + note + " was out of range. Setting to C4 Quarter note.");
+				System.exit(0);
+			}
+			else if (note.matches("[A,B]{1}[#]?[9]{1}")) { //checking for A9, A#9, and B9
+				setNoteDefault();
+				System.out.println("Your note " + note + " was out of range. Setting to C4 Quarter note.");
+				System.exit(0);
+			}
+			else if (note.equals("G#9")){ //couldn't do above cause G9 is valid, but G#9 isn't 
+				setNoteDefault();
+				System.out.println("Your note " + note + " was out of range. Setting to C4 Quarter note.");
+				System.exit(0);
+			}
+			else{
+				
+			}
+		}
+		else {
+			System.out.println("Your note " + note + " was out of range. Setting to C4 Quarter note.");
+			setNoteDefault();
+			System.exit(0);
+		}
+	}
+	
+	public MIDI(int midiNumber, Length length){
+		
+	}
+	
+	public MIDI (double frequency, Length length){
+		
+	}
+	
+	private void setNoteDefault(){
+		this.midiNumber = 60;
+		this.length = Length.QUARTER;
+	}
+	
+	public void setFrequency(double frequency){
+		
+	}
+	
+	public double getFrequency(){ //Hz
+		
+	}
+	
+	public void setLength(Length length){
+		
+	}
+	
+	public Length getLength(){
+		
+	}
+	
+	public void setMidiNumber(int midi){
+		
+	}
+	
+	public int getMidiNumber(){
+		
+	}
+	
+	public void setOctave(int octave){
+		
+	}
+	
+	public int getOctave(){
+		
+	}
+	
+	public void setSharp(){
+		
+	}
+	
+	public boolean isSharp(){
+		return false;
+	}
+	
+	public String getNote(){
+		
+	}
+	
+	public String toString(){
+		
+	}
+
+
+
+
+
+
+
+
+
 
 }
