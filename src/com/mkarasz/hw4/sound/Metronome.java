d6cf29f7749e6985308827a628a70bd620738c56
DraftiestHat <mattchris9@hotmail.com>
2015-02-21 23:52:32 -0700

    Added sound main file and finished metronome
    This one was all on me. I have no clue if I did it right.

--- src/com/mkarasz/hw4/sound/Metronome.java
+++ src/com/mkarasz/hw4/sound/Metronome.java
@@ -1,134 +1,51 @@
 package com.mkarasz.hw4.sound;
 
-import java.util.Scanner;
-
-import com.mkarasz.hw4.sound.MIDI.Length;
-
-
 public class Metronome {
 	
-	MIDI midiNote;
+	MIDI note;
 	double bps;
 	
-	public void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String userChoice;
-		String note = null;
-		double frequency = 0.0;
-		double length = 0.0;
-		Length lenEntered = Length.QUARTER; 
-		int mNum = -1;
-		double duration = 0.0;
-		
-		System.out.println("Would you like to enter a frequency, a note in SPN form, or a MIDI number?");
-		System.out.println("F - frequency, S - SPN form, M - MIDI");
+	public Metronome() {
+		this.note = new MIDI(); 
+		this.bps = 60;
+	}
 	
-		userChoice = in.nextLine();
-		
-		if(userChoice.equalsIgnoreCase("F")){
-			System.out.println("Please enter the frequency:");
-			if(in.hasNextDouble()){
-				frequency = in.nextDouble();
-			}
-			else
-				System.out.println("You entered something wrong. Please start over.");
-		}
-		else if (userChoice.equalsIgnoreCase("S")){
-			System.out.println("Please enter the note in SPN form:");
-			note = in.nextLine();
-		}
-		else if (userChoice.equalsIgnoreCase("M")){
-			System.out.println("Please enter the MIDI number:");
-			if(in.hasNextInt()){
-				mNum = in.nextInt();
-			}
-			else
-				System.out.println("You entered something wrong. Please start over.");
-		}
-		else {
-			System.out.println("Please choose a one letter input from the above values.");
-			System.exit(1);
-		}
-		
-		System.out.println("Please enter a length for the note.");
-		System.out.println("e.g. 1, 1/2, 1/4, 1/8, 1/16, 1/32, 1/64");
-		
-		if(in.hasNextDouble()){
-			length = in.nextDouble();
-			if (length != 1 || length != 0.5 || length != .25 || length != 1/8 || length != 1/16 || length != 1/32 || length != 1/64){
-				System.out.println("You entered something wrong. Try again!");
-				System.exit(1);
-			}
-			else {
-				if(length == 1){
-					lenEntered = Length.WHOLE;
-				}
-				else if (length == 0.5){
-					lenEntered = Length.HALF;
-				}
-				else if (length == 0.25){
-					lenEntered = Length.QUARTER;
-				}
-				else if (length == 1/8){
-					lenEntered = Length.EIGHTH;
-				}
-				else if (length == 1/16){
-					lenEntered = Length.SIXTEENTH;
-				}
-				else if (length == 1/32){
-					lenEntered = Length.THIRTYSECONDTH;
-				}
-				else if (length == 1/64){
-					lenEntered = Length.SIXTYFOURTH;
-				}
-				else{
-					System.out.println("You entered something wrong. Try again!");
-					System.exit(1);
-				}
-			}
-		}
-		
-		if(userChoice.equalsIgnoreCase("F")){
-			setMidiNote(new MIDI(frequency, lenEntered));
-		}
-		else if (userChoice.equalsIgnoreCase("S")){
-			setMidiNote(new MIDI(note, lenEntered));
-		}
-		else {
-			setMidiNote(new MIDI(mNum,lenEntered));
-		}
-			
-		System.out.println("What are the beats per minute?");
-		
-		if(in.hasNextDouble()){
-			this.bps = in.nextDouble()/60;
-		}
-		else {
-			System.out.println("You enetered something wrong. Please try again!");
-			System.exit(0);
-		}
-		
-		duration = calcSeconds();
-		
-		System.out.println("The duration is " + duration + ".");
-		
+	public Metronome(double bps, MIDI note){
+		this.note = note;
+		this.bps = bps;
+	}	
 	
 	
-	
-		in.close();
-	
+	/**
+	 * @return the note
+	 */
+	public MIDI getNote() {
+		return note;
 	}
 
-	public MIDI getMidiNote() {
-		return midiNote;
+	/**
+	 * @param note the note to set
+	 */
+	public void setNote(MIDI note) {
+		this.note = note;
 	}
 
-	public void setMidiNote(MIDI midiNote) {
-		this.midiNote = midiNote;
+	/**
+	 * @return the bps
+	 */
+	public double getBps() {
+		return bps;
 	}
-	
-	private double calcSeconds(){
-		switch (midiNote.getLength()){
+
+	/**
+	 * @param bps the bps to set
+	 */
+	public void setBps(double bps) {
+		this.bps = bps;
+	}
+
+	public double calcSeconds(){
+		switch (note.getLength()){
 		case WHOLE:
 			return 4 / this.bps;
 		case HALF:
---
+++ src/com/mkarasz/hw4/sound/Sound.java
@@ -0,0 +1,125 @@
+package com.mkarasz.hw4.sound;
+
+import java.util.Scanner;
+
+import com.mkarasz.hw4.sound.MIDI.Length;
+
+public class Sound {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Scanner inTwo = new Scanner(System.in);
+		String userChoice;
+		String note = null;
+		double frequency = 0.0;
+		double length = 0.0;
+		Length lenEntered = Length.QUARTER; 
+		int mNum = -1;
+		MIDI noteMIDI = null;
+		double bps = 0.0;
+		Metronome met = null;
+		
+		System.out.println("Would you like to enter a frequency, a note in SPN form, or a MIDI number?");
+		System.out.println("F - frequency, S - SPN form, M - MIDI");
+	
+		userChoice = in.nextLine();
+		
+		if(userChoice.equalsIgnoreCase("F")){
+			System.out.println("Please enter the frequency:");
+			if(in.hasNextDouble()){
+				frequency = in.nextDouble();
+			}
+			else
+				System.out.println("You entered something wrong. Please start over.");
+		}
+		else if (userChoice.equalsIgnoreCase("S")){
+			System.out.println("Please enter the note in SPN form:");
+			note = in.nextLine();
+		}
+		else if (userChoice.equalsIgnoreCase("M")){
+			System.out.println("Please enter the MIDI number:");
+			if(in.hasNextInt()){
+				mNum = in.nextInt();
+			}
+			else
+				System.out.println("You entered something wrong. Please start over.");
+		}
+		else {
+			System.out.println("Please choose a one letter input from the above values.");
+			System.exit(1);
+		}
+		
+		System.out.println("Please enter a length for the note.");
+		System.out.println("e.g. 1, 1/2, 1/4, 1/8, 1/16, 1/32, 1/64");
+		
+		if(in.hasNextDouble()){
+			length = in.nextDouble();
+			//System.out.println("" + length + " length");
+			/*if (length != 1 || length != 0.5 || length != .25 || length != (1/8) || length != (1/16) || length != (1/32) || length != (1/64)){
+				System.out.println("You entered something wrong. Try again!");
+				System.exit(1);
+			}
+			else {*/
+				if(length == 1){
+					lenEntered = Length.WHOLE;
+				}
+				else if (length == 0.5){
+					lenEntered = Length.HALF;
+				}
+				else if (length == 0.25){
+					lenEntered = Length.QUARTER;
+				}
+				else if (length == 1/8){
+					lenEntered = Length.EIGHTH;
+				}
+				else if (length == 1/16){
+					lenEntered = Length.SIXTEENTH;
+				}
+				else if (length == 1/32){
+					lenEntered = Length.THIRTYSECONDTH;
+				}
+				else if (length == 1/64){
+					lenEntered = Length.SIXTYFOURTH;
+				}
+				else{
+					System.out.println("You entered something wrong. Try again!");
+					System.exit(1);
+				}
+			//}
+		}
+		//System.out.println("" + length + " length2");
+		if(userChoice.equalsIgnoreCase("F")){
+			noteMIDI = new MIDI(frequency, lenEntered);
+		}
+		else if (userChoice.equalsIgnoreCase("S")){
+			noteMIDI = new MIDI(note, lenEntered);
+		}
+		else {
+			noteMIDI = new MIDI(mNum,lenEntered);
+		}
+			
+		System.out.println("What are the beats per minute?");
+		
+		if(inTwo.hasNextInt()){
+			bps = (double)inTwo.nextInt();
+			bps = bps / 60;
+			System.out.println("bps: " + bps);
+		}
+		else {
+			System.out.println("You enetered something wrong. Please try again!");
+			System.exit(0);
+		}
+		
+		met = new Metronome(bps, noteMIDI);
+		
+		System.out.println("The duration is " + met.calcSeconds() + " seconds.");
+		
+	
+	
+	
+		in.close();
+		inTwo.close();
+
+	}
+
+}
